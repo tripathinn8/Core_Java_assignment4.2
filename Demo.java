c1c404aeaac505168ced7fa992fc3e59613ae57d
tripathinn8 <30956659+tripathinn8@users.noreply.github.com>
2017-08-14 19:40:08 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,19 @@
+
+public class Demo 
+{
+
+	public void difference()
+	{
+		System.out.println("Hello all!!");
+	}
+	
+	private void m1()
+	{
+		System.out.println("Good bye!!");
+	}
+	public static void main(String[] args)
+	{
+		Demo dem= new Demo();
+		dem.m1();
+	}
+}
---
+++ Demo1.java
@@ -0,0 +1,13 @@
+
+public class Demo1 extends Demo
+{
+		
+	public static void main(String[] args) 
+	{
+		Demo1 obj= new Demo1();
+		obj.difference();
+		//can call the method difference() by object obj
+		//cannot call private member m1() by the object obj
+	}
+
+}
